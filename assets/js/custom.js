121207163f1047f9bf5997f035038d591b9be331
hadung2002 <79574082+hoanhdung1112002@users.noreply.github.com>
2024-09-26 14:38:56 +0700

    update file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,24 @@
 jQuery(function($) {
 	const $btnOpen = $(".menu-open")
 	const $btnClose = $(".menu-close");
+  const $overlay = $(".overlay");
 	var $menuBar = $(".menu")
 	$btnOpen.click(function(e) {
 		e.preventDefault();
 		$menuBar.addClass("show");
+    $overlay.addClass("active");
 	});
 
 	$btnClose.click(function(e) {
 		e.preventDefault();
 		$menuBar.removeClass("show");
+    $overlay.removeClass("active");
 	});
 
 	$(document).click(function(e) {
 		if (!$(e.target).closest('.menu, .menu-open').length) {
 			$menuBar.removeClass("show");
+      $overlay.removeClass("active");
 		}
 	});
 	var windowHeight = $(window).height();
