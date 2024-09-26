a3ac0756e0e6ad654fd005d5dc1169648b9735de
hadung2002 <79574082+hoanhdung1112002@users.noreply.github.com>
2024-09-26 11:51:20 +0700

    update file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,4 +17,25 @@ jQuery(function($) {
 			$menuBar.removeClass("show");
 		}
 	});
+	var windowHeight = $(window).height();
+        $(window).scroll(function() {
+          var scrollPosition = $(window).scrollTop();
+  
+          $('.animated').each(function() {
+            var elementPosition = $(this).offset().top;
+            
+            if (scrollPosition > elementPosition - windowHeight / 1.6) {
+              $(this).addClass('run-animated');
+            }
+          });
+        });
+        function animatedFlash() {
+          setInterval(function () {
+            $('.animated-flash').addClass('run-animated');
+          }, 2000);
+          setInterval(function () {
+            $('.animated-flash').removeClass('run-animated');
+          }, 3000);
+        }
+        animatedFlash();
 });
