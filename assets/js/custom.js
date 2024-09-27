b8c2529325062e17bf9ded9704be60711e05e89d
hadung2002 <79574082+hoanhdung1112002@users.noreply.github.com>
2024-09-27 11:26:53 +0700

    update file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,8 @@
 jQuery(function($) {
 	const $btnOpen = $(".menu-open")
-	const $btnClose = $(".menu-close");
+	const $btnClose = $(".navbar-close");
   const $overlay = $(".overlay");
-	var $menuBar = $(".menu")
+	var $menuBar = $(".navbar")
 	$btnOpen.click(function(e) {
 		e.preventDefault();
 		$menuBar.addClass("show");
@@ -16,31 +16,9 @@ jQuery(function($) {
 	});
 
 	$(document).click(function(e) {
-		if (!$(e.target).closest('.menu, .menu-open').length) {
+		if (!$(e.target).closest('.navbar, .menu-open').length) {
 			$menuBar.removeClass("show");
       $overlay.removeClass("active");
 		}
 	});
-  
-	var windowHeight = $(window).height();
-        $(window).scroll(function() {
-          var scrollPosition = $(window).scrollTop();
-  
-          $('.animated').each(function() {
-            var elementPosition = $(this).offset().top;
-            
-            if (scrollPosition > elementPosition - windowHeight / 1.6) {
-              $(this).addClass('run-animated');
-            }
-          });
-        });
-        function animatedFlash() {
-          setInterval(function () {
-            $('.animated-flash').addClass('run-animated');
-          }, 2000);
-          setInterval(function () {
-            $('.animated-flash').removeClass('run-animated');
-          }, 3000);
-        }
-        animatedFlash();
 });
